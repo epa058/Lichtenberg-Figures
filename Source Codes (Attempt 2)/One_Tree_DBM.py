824bdd9d202cf899ea050b6dd87ae85ba42330da
Simon <epa058@gmail.com>
2023-07-03 17:49:58 -0400

    Update One_Tree_DBM.py

--- Source Codes (Attempt 2)/One_Tree_DBM.py
+++ Source Codes (Attempt 2)/One_Tree_DBM.py
@@ -4,39 +4,67 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 
-# Create an n x n grid of zeros
-n = 21
-grid = np.zeros((n, n))
+# Create a grid of zeros
+print('Enter a number of rows, e.g.: 100:')
+x_row = int(input())
+
+print('Enter a number of columns, e.g.: 100:')
+y_col = int(input())
+
+# Anisotropy generator
+print('Include anisotropies? (Yes / No):')
+anis = str(input()).upper()
+
+if anis == "YES":
+    grid = np.random.randint(0, 2, size=(x_row, y_col))
+
+else:
+    grid = np.zeros((x_row, y_col))
+
+# Set boundary conditions
+grid[0, :] = 0
+grid[-1, :] = 10
 
 # Create a list of growth points
 growth = []
 
-# Set boundary conditions
-grid[0, :] = 0
-grid[-1, :] = 1
+# Set the growth site
+growRow = 0
+growCol = y_col // 2
+growPt = (growRow, growCol)
+#grid[growPt] = 0
+growth.append(growPt)
+'''
+print('Enter a starting position, e.g.: 0, 0:')
+x_input, y_input = input().split(",")
+x_input = int(x_input)
+y_input = int(y_input)
+'''
 
 # Initializing Laplace's equation on the grid
 iterations = 100
 for _ in range(iterations):
-    for i in range(1, n-1): # Keeps the first and last row values fixed
-        for j in range(0, n):
+    for i in range(1, x_row-1): # Keeps the first and last row values fixed
+        for j in range(0, y_col):
             up = grid[i-1, j]
             down = grid[i+1, j]
-            left = grid[i, (j-1) % n]
-            right = grid[i, (j+1) % n]
+            left = grid[i, (j-1) % y_col]
+            right = grid[i, (j+1) % y_col]
             grid[i, j] = (left + right + up + down) / 4
+print(grid)
 
 # Define the Laplace operator
 def laplaceOperator(grid):
-    n = len(grid[0])
+    x_row = len(grid)
+    y_col = len(grid[0])
     newGrid = grid.copy()
-    for i in range(1, n - 1): # Keeps the first and last row values fixed
-        for j in range(0, n):
+    for i in range(1, x_row - 1): # Keeps the first and last row values fixed
+        for j in range(0, y_col):
             if (i, j) not in growth:
                 up = grid[i-1, j]
                 down = grid[i+1, j]
-                left = grid[i, (j-1) % n]
-                right = grid[i, (j+1) % n]
+                left = grid[i, (j-1) % y_col]
+                right = grid[i, (j+1) % y_col]
                 newGrid[i, j] = (left + right + up + down) / 4
     return newGrid
 
@@ -44,19 +72,21 @@ def laplaceOperator(grid):
 def laplaceEquation(grid, iterations = 100):
     for _ in range(iterations):
         grid = laplaceOperator(grid)
+    print(grid)
     return grid
 
 def simulation(grid):
     global growth
-    n = len(grid[0])
+    x_row = len(grid)
+    y_col = len(grid[0])
     
     # Find all possible growth sites
     possibleSites = []
     for i, j in growth:
         for delta_i, delta_j in [(-1, 0), (1, 0), (0, -1), (0, 1)]:  # Displacements: up, down, left, right
-            new_i, new_j = i + delta_i, (j + delta_j) % n
+            new_i, new_j = i + delta_i, (j + delta_j) % y_col
             
-            if 0 < new_i < n and (new_i, new_j) not in growth:
+            if 0 < new_i < x_row and (new_i, new_j) not in growth:
                 possibleSites.append((new_i, new_j))
 
     # Terminate if the algorithm is stuck
@@ -66,7 +96,7 @@ def simulation(grid):
     # Calculate growth probability for each possible growth site
     probabilities = []
     for i, j in possibleSites:
-        prob = grid[i, j]**2
+        prob = grid[i, j] # you can square or square root this
         probabilities.append(prob)
 
     # Normalize probabilities
@@ -83,7 +113,7 @@ def simulation(grid):
     # Add new growth site to growth list
     grid[newPoint] = 0
     growth.append(newPoint)
-    if newPoint[0] == n - 1:
+    if newPoint[0] == x_row - 1:
         return grid
 
     # Update the grid and recursively call the simulation function
@@ -92,21 +122,15 @@ def simulation(grid):
 
     return grid
 
-# Set the growth site
-growRow, growCol = 0, n // 2
-growPt = (growRow, growCol)
-#grid[growPt] = 0
-growth.append(growPt)
-
 # Begin the simulation
 grid = simulation(grid)
 
-
 # Results
 print(grid)
+print(growth)
 
 # Cleaner results
-newGrid = np.zeros((n, n))
+Grid = np.zeros((x_row, y_col))
     
 # Animation
 def animate(i): 
@@ -114,13 +138,13 @@ def animate(i):
         print(i)
         x = growth[i][0]
         y = growth[i][1]
-        newGrid[x][y] = i + 100
+        Grid[x][y] = i + 100
         ax.clear()
-        ax.matshow(newGrid, cmap='Blues')
+        ax.matshow(Grid, cmap='Blues')
 
     except IndexError:
         print("Done")
-        return newGrid
+        return Grid
 
 fig, ax = plt.subplots()
 
